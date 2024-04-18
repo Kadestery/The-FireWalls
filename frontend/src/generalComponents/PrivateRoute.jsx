8688c9c182b41c41da4354524ed5aa82b2896cc5
Ishaï Cohen <91989855+L3CH3R0@users.noreply.github.com>
2024-04-18 13:37:36 -0400

    Update PrivateRoute.jsx

--- frontend/src/generalComponents/PrivateRoute.jsx
+++ frontend/src/generalComponents/PrivateRoute.jsx
@@ -1,5 +1,6 @@
 import { Navigate } from "react-router-dom";
 
+
 // eslint-disable-next-line react/prop-types
 export default function PrivateRoute({ children }) {
   return localStorage.getItem("email") ? children : <Navigate to="/login" />;
