dc0b7dc10c012453a5c82b34e328559d2f4ceb33
David Onwionoko <61207991+d-realblank@users.noreply.github.com>
2024-04-17 23:02:38 -0400

    Update roomRoutes.py

--- backend/app/routes/roomRoutes.py
+++ backend/app/routes/roomRoutes.py
@@ -13,7 +13,7 @@ from typing import List
 @router.get("/getrooms", response_model=List[schemas.RoomState])
 async def get_rooms(house_id: int, db: Session = Depends(get_db)):
     rooms = get_rooms_in_house(db, house_id)
-    # Filter the room objects to only include the desired fields
+    # Filter the room objects, include desired fields only
     filtered_rooms = [ { "room_id": room.room_id, "profiles_in_room": [ {"profile_id": profile.profile_id, "profile_username": profile.profile_username, "profile_type": profile.profile_type} for profile in room.profiles ], "name": room.name, "room_type": room.room_type, "window_state": room.window_state, "door_state": room.door_state, "light_state": room.light_state } for room in rooms ]
     return filtered_rooms
 
