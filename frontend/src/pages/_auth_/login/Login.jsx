4372d586eb7ac3577eef6cafe51811c352c07125
Kade Keating <97750132+Kadestery@users.noreply.github.com>
2024-04-19 16:31:31 -0400

    Update Login.jsx

--- frontend/src/pages/_auth_/login/Login.jsx
+++ frontend/src/pages/_auth_/login/Login.jsx
@@ -1,6 +1,6 @@
 import AuthFormHeader from "../authComponents/AuthFormHeader";
 import { Form } from "react-router-dom";
-
+//Authorization for login header
 function Login() {
   return (
     <div className="h-screen flex justify-center items-center flex-col">
