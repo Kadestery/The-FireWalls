801b56c920a53465f3c2420397a145686c8a6658
j3rrimmy <98069409+j3rrimmy@users.noreply.github.com>
2024-04-21 22:44:51 -0400

    Update observer.py
    added remaining temperature sensor and user interface

--- backend/app/designPatterns/ObserverPattern/observer.py
+++ backend/app/designPatterns/ObserverPattern/observer.py
@@ -17,3 +17,20 @@ class Subject:
     def notify_observers(self, data):
         for observer in self._observers:
             observer.update(data)
+
+
+class TemperatureSensor(Subject):
+    def set_temperature(self, temperature):
+        # Simulate temperature measurement and notify observers
+        print(f"TemperatureSensor: temperature has been set to {temperature}°C.")
+        self.notify_observers(temperature)
+
+class UserInterface(Observer):
+    def update(self, temperature):
+        print(f"UserInterface: Temperature changed to {temperature}°C")
+        if temperature <= 0:
+            self.alert(temperature)
+
+    def alert(self, temperature):
+        print(f"UserInterface Alert: Temperature is {temperature}°C! Beware of freezing conditions.")
+
