989c06ea05f010499cc466dda8cb4368c6c354df
Ishaï Cohen <91989855+L3CH3R0@users.noreply.github.com>
2024-04-21 23:38:16 -0400

    Create test_strategy.py
    added test for parent permission

---
+++ backend/tests/test_strategy.py
@@ -0,0 +1,19 @@
+import unittest
+from unittest.mock import MagicMock
+
+from fastapi import HTTPException
+
+from backend.app.DB.schemas import ActionType
+from backend.app.designPatterns.StrategyPattern.permission_strategies import ParentPermissionStrategy, \
+    ChildPermissionStrategy, GuestPermissionStrategy, StrangerPermissionStrategy, Context
+# Assuming you've imported all necessary classes and ActionType from your actual project structure
+
+from backend.app.designPatterns.StrategyPattern.strategy_methods import get_permissions
+
+
+class TestPermissionStrategies(unittest.TestCase):
+    def test_parent_permission_strategy(self):
+        strategy = ParentPermissionStrategy()
+        self.assertTrue(strategy.has_permission(ActionType.CHANGE_LIGHT))
+        self.assertTrue(strategy.has_permission(ActionType.CHANGE_WINDOW))
+        self.assertTrue(strategy.has_permission(ActionType.CHANGE_DOOR))
