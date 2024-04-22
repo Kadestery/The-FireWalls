f4344a168fb3fc37ea9c8ab90ca6d2b240792a6f
j3rrimmy <98069409+j3rrimmy@users.noreply.github.com>
2024-04-21 22:44:18 -0400

    Create observer.py
    added the observer interface and the subject

---
+++ backend/app/designPatterns/ObserverPattern/observer.py
@@ -0,0 +1,19 @@
+
+class Observer:
+    def update(self, data):
+        """Receive update from subject."""
+        pass
+
+class Subject:
+    def __init__(self):
+        self._observers = []
+
+    def add_observer(self, observer):
+        self._observers.append(observer)
+
+    def remove_observer(self, observer):
+        self._observers.remove(observer)
+
+    def notify_observers(self, data):
+        for observer in self._observers:
+            observer.update(data)
